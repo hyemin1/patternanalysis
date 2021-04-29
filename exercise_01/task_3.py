129013d968808f8266180c8592eb27de8d314376
j-krieg <54662616+j-krieg@users.noreply.github.com>
2021-04-30 00:16:37 +0200

    Update task_3.py
    work in progress

--- exercise_01/task_3.py
+++ exercise_01/task_3.py
@@ -12,60 +12,96 @@ This is an addition to task 2:
 - 1. We have generate  10.000, 50.000, 100.000, 200.000, and 400.000 samples from the racoon image and do not access the original racoon image anymore.
 - 2. Then reconstruct the image with the Parzen Window Estimator the sampling the densities  for each of the five previously generated data sets.
 - 3. Plot the output comprising of the density we got from the Parzen window estimator.
+
+This is how the Parzen estimator works:
+whole density: p(x) = 1/N sum(i=1..N) [(1/h^D)*k* ((x - x_i)/h)]
+    -D: dimension
+    -h: window size
+    
 """
 ####################
 
 
+def racoon_sample (sample_num):
+    # generates sample_num from the racoon image
+    
+    ### this comes from task 2:
+    raccoon = scipy.misc.face(gray=True)
+    # gray coloring
+    plt.gray()
+    # apply gaussian filter
+    raccoon = gaussian_filter(raccoon, sigma=3)
+    # image display-- original raccon image
+    plt.imshow(raccoon)
+    # get rid of the axis
+    plt.axis('off')
+    plt.show()
+
+    # convert 2D array to 1D
+    raccoon = raccoon.flatten()
+    # compute CDF
+    raccoon_cdf = raccoon.cumsum()
+    #normalization skipped
 
 
-# load the racoon image
-raccoon = scipy.misc.face(gray=True)
-# gray coloring
-plt.gray()
-# apply gaussian filter
-raccoon = gaussian_filter(raccoon, sigma=3)
-# image display-- original raccon image
-plt.imshow(raccoon)
-# get rid of the axis
-plt.axis('off')
-plt.show()
-
-# convert 2D array to 1D
-raccoon = raccoon.flatten()
-# compute CDF
-raccoon_cdf = raccoon.cumsum()
-#normalization skipped
+    # compute sampled array
+    # output array (1-D)
+    output= [0]*len(raccoon_cdf)
 
 
-# compute sampled array
-# output array (1-D)
-output= [0]*len(raccoon_cdf)
-# number of samples
-sample_num = 500000
+    #generate random, uniform samples
+    u_data = np.random.uniform(0, max(raccoon_cdf), sample_num)
+    # sort u_data in non-decreasing order
+    u_data = sorted(u_data)
 
-#generate random, uniform samples
-u_data = np.random.uniform(0, max(raccoon_cdf), sample_num)
-# sort u_data in non-decreasing order
-u_data = sorted(u_data)
+    #u_crr for indexing u_data
+    u_crr=0
 
-#u_crr for indexing u_data
-u_crr=0
+    #loop through CDF
+    for x in range(len(raccoon_cdf)):
+        #if we checked all values in u_data, end loop
+        if u_crr==len(u_data):
+            break
+        #if current CDF value is equal or bigger than current value of u_data
+        if(raccoon_cdf[x]>=u_data[u_crr]):
+            output[x]=raccoon_cdf[x]
+            u_crr+=1
+            
+    return output
 
-#loop through CDF
-for x in range(len(raccoon_cdf)):
-    #if we checked all values in u_data, end loop
-    if u_crr==len(u_data):
-        break
-    #if current CDF value is equal or bigger than current value of u_data
-    if(raccoon_cdf[x]>=u_data[u_crr]):
-        output[x]=raccoon_cdf[x]
-        u_crr+=1
+
+def parzen_reconstruct (input):
+    # reconstructs a sampled image using the parzen window estimator
+    h = 123
+    D = 2
+    
+    """
+    todo...
+    whole density: p(x) = 1/N sum(i=1..N) [(1/h^D)*k* ((x - x_i)/h)]    
+    """
+    
+   return 0 # todo...
+
+
+
+recon10k = parzen_reconstruct(racoon_sample (10000))
+recon50k = parzen_reconstruct(racoon_sample (50000))
+recon100k = parzen_reconstruct(racoon_sample (100000))
+recon200k = parzen_reconstruct(racoon_sample (20000))
+recon500k = parzen_reconstruct(racoon_sample (400000))
+
+
+
 
 #reshape array to 2-D
-output=np.reshape(output,(768,1024))
+output10k=np.reshape(recon10k,(768,1024))
+output50k=np.reshape(recon50k,(768,1024))
+output100k=np.reshape(recon100k,(768,1024))
+output200k=np.reshape(recon200k,(768,1024))
+output500k=np.reshape(recon500k,(768,1024))
+
 
 #display
 plt.figure()
-plt.imshow(output)
-
+plt.imshow(output500k)
 plt.show()
