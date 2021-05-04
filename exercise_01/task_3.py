13291749523cfc477be7f18bb69bc1a23cf4db12
j-krieg <54662616+j-krieg@users.noreply.github.com>
2021-05-04 23:25:10 +0200

    task 3 ready

--- exercise_01/task_3.py
+++ exercise_01/task_3.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 import bisect as bin
 import numpy as np
+import random as random
 from sklearn import preprocessing as pre
-
 
 ####################
 """
@@ -17,15 +17,17 @@ This is how the Parzen estimator works:
 whole density: p(x) = 1/N sum(i=1..N) [(1/h^D)*k* ((x - x_i)/h)]
     -D: dimension
     -h: window size
-    
+
 """
+
+
 ####################
 
 
-def racoon_sample (sample_num):
+def raccoon_sample(sample_num):
     # generates sample_num samples from a probability distribution obtained from brightness values of a racoon image turned into 1 1D distribution
-    # note: output is 1D
-    
+    # note: output is 2D with 0 or 255 brightness value.
+
     ### this comes from task 2:
     raccoon = scipy.misc.face(gray=True)
     # gray coloring
@@ -33,160 +35,157 @@ def racoon_sample (sample_num):
     # apply gaussian filter
     raccoon = gaussian_filter(raccoon, sigma=3)
 
-    """
-    # image display-- original raccon image
-    plt.imshow(raccoon)
-    # get rid of the axis
-    plt.axis('off')
-    plt.show()
-    """
+    myplot = plt.imshow(raccoon)
+    # plotImage (reconstructedImage)
+    myplot.figure.savefig("raccoon_gaussian3sigma_original.png")
 
-    # convert 2D array to 1D
-    raccoon = raccoon.flatten()
-    # compute CDF
-    raccoon_cdf = raccoon.cumsum()
-    #normalization skipped
+    # print(len(raccoon)) # => 768
+    # print(len(raccoon[0]))  # => 1024
+    # print(min(raccoon[100]))
+    # print(max(raccoon[100]))
 
+    #### this is how it looks like: racoon[768][1024] with values 0..255
 
-    # compute sampled array
-    # output array (1-D)
-    output= [0]*len(raccoon_cdf)
+    output = np.zeros((768, 1024))
+    for x in range(0,768):
+        for y in range(0,1024):
+            output[x][y] = -1
 
+    #draw random samples: output[768][1024] is -1 if no sample is drawn an 0..255 if it is one of the grayscale values originating from the raccoon image
+    s = 0
+    while (s < sample_num):
+        x = np.random.randint(0, 768-1)
+        y = np.random.randint (0, 1024-1)
+        if output[x][y] == -1: # pixel not used yet
+            output[x][y] = 0
+            if random.random() >= float(raccoon[x][y])/255.: # probability depends on brightness value
+                output[x][y] = 255
+                s+=1
 
-    #generate random, uniform samples
-    u_data = np.random.uniform(0, max(raccoon_cdf), sample_num)
-    # sort u_data in non-decreasing order
-    u_data = sorted(u_data)
-
-    #u_crr for indexing u_data
-    u_crr=0
-
-    #loop through CDF
-    for x in range(len(raccoon_cdf)):
-        #if we checked all values in u_data, end loop
-        if u_crr==len(u_data):
-            break
-        #if current CDF value is equal or bigger than current value of u_data
-        if(raccoon_cdf[x]>=u_data[u_crr]):
-            output[x]=raccoon_cdf[x]
-            u_crr+=1
-
-    # output is an array containing 1D positions (they came from 2D->1D by generating a pdf from the racoon image) of where we sampled the PDF.
     return output
 
 
-def parzen_reconstruct (input, windowSize):
+def plotImage (image):
+    # typical input: image[768][1024] wih integer values 0..255
+    # pixel values of -1 are also okay and just displayed black
+
+
+    # display
+    plt.figure()
+    plt.imshow(image)
+
+    plt.show()
+
+
+
+
+def parzen_reconstruct(input, windowSize):
     # reconstructs a sampled PDF using the parzen window estimator
     # This method is called Kernel Density Estimation (KDE) and helps us to estimate a PDF from a finite dataset.
     #           Parzen Window Estimator is a "non-parametric" way, i.e. that we do not assume a fixed underlying distribution such as a Gaussian.
-    # Input is a 1D array containing grayscale values at positions that were sampled and zeros at positions that have not been sampled.
-    #       - A 1D position can later be transformed into 2D again.
-    #       - This is an example of how the input data looks like: [0, 0, 0, (...), 0, 0, 88148563, 0, 0, 0, 0, 0, 0, 88148812, 0, 88148123, 0, 0, 0, 0, 0]
-    # Output is a 1D array of the same size like the input. Instead of the black spots that were represented by zero entries,
-    # this function calculated how the data should actually look like based on the Parzen Window Estimator.
-    #       - This array represents a PDF
-    #       - It can be turned into a 2D array as their 1D coordinate number transforms into a pair of x-y-coordinates
-    #       - I.e., the PDF of which we only had a limited number of samples has been reconstructed and can be turned into an image again.
+    # Input is a 2D array containing values 0 (no sample) or 255 (sample)
+    #       - This how the input data looks like: input[768][1024] with values 0 or 255
+    #       - We just consider this input array as sample of a 2D PDF and want to reconstruct the PDF
+    # Output is the same 2D array with the same dimensions like input with and intensity values 0..255 as reconstructed by the Parzen Window Estimator that comprise PDF values.
 
-    N = len (input) # N is the number of all available samples
+
+    height = len(input) # 768
+    width = len(input[0]) # 1024
+
+    print ("height: " + str(height))
+    print ("width: " + str(width))
+
+
+
+    D = 2 # 2-dimenstional
     h = windowSize
-    V = pow(h, 1) # Volume of the hypercube V=h^D, in this case of 1D: V=h
+    V = pow(h, D)  # Volume of the hypercube V=h^D, in this case of 1D: V=h
 
+    print("hypercube volume: " + str(V))
+    print("windowSize: " + str(windowSize))
 
-    # the while loop below implements the folowing formula: p(x_i) = (1/N)* sum_{i=1..N} ((1/V) * Psi(x_i)*((x - x_i)/h))
+    totalSamples = 0
+    for x in range(0, height):
+        for y in range(0, width):
+            if (input[x][y] == 255): ## brightness value is either 0 or 255
+                totalSamples += 1
+
+    print ("total number of samples: " + str(totalSamples))
+
+    # the while loop below implements the following formula: p(x_i) = (1/N)* sum_{i=1..N} ((1/V) * Psi(x_i)*((x - x_i)/h))
     # with
     #   x: center of the hypercube
-    #   x_i: PDF value of one sample
-    #   i=0..N-1 with N being the number of available samples
-    #   V=h^D as the volume of the hypercube, in case of 1D: V=h
-    #   Psi is just a function that is 1 if x_i is within the hypercube (i.e. all components of the vector are included); zero if outside.
-    # so practically, for one hyperbox we can calculate p(x) = (1/(N*V)) * sum_{over all x_i in the hyperbox} (x - x_i) / h
-    # then we assign this p(x) value to all x_i that are within the hyperbox
-    # the only problem is that the last window might be smaller than h because there is no more data. That's why we might use another window size for this exception
-    windowRight = windowSize - 1
+    #   x_i: position of one sample
 
-    H = int(N / h)
-    if N%h: H+=1 #  we need to account for the last window that might be a little smaller than intented windowSize
+    # Psi is just a function that is 1 if x_i is within the hypercube (i.e. all components of the vector are included); zero if outside.
+    # We need to account for the problem that a dimension ("D: width, height) is not divisible by the window size. This is why we might use another window size for this exception.
 
-    output = input # we're just copying this to avoid operating on input[] while we're still reading it - should be fine but just to be safe.
-    windowLeft = 0
-    for b in range (0, H): # iterate over all boxes
-        windowRight = windowLeft + h - 1
-        if (windowRight >= N):
-            windowRight = N - 1
-        currentWindowSize = (windowRight - windowLeft) + 1 # in most cases, this is the original window size h; during the last iteration it migh tbe smaller
-        boxCenter = (windowRight + windowLeft) / 2. # does not need to be an integer
-
-        # now we calculate the box value and iterate over all samples within the hyperbox - this covers the Psi part of the equation
-        boxValue = 0
-        for i in range (windowLeft, windowRight + 1):
-            boxValue += (boxCenter - i) / currentWindowSize ### PLEASE CHECK: do we need to multiply this with input[i]? Somehow we need to get in the grayscale values. Feels right but I can't find this in the formula.
-            #boxValue += (boxCenter - i) / currentWindowSize
-
-        # now we assign the box value to all the points within the current window
-        for i in range (windowLeft, windowRight + 1):
-            output[i] = boxValue*input[i]
+    # we use this formula to calculate the pdf value for a given hyperbox: p(x) = k / (n*V) with k as the number of samples within a hyperbox. V the volume of the hyperbox and n the total number of samples.
 
 
-        windowLeft += h #  for the next iteration
+    boxesX = height / windowSize
+    if float(int(boxesX)) != boxesX: ## in case image dimension is not divisible by window size, we need to account for the small rest (later, we assign an alternative, smaller window size for this case)
+        boxesX = int(boxesX) + 1
+
+    boxesY = width / windowSize
+    if float(int(boxesY)) != boxesY: ## in case image dimension is not divisible by window size, we need to account for the small rest (later, we assign an alternative, smaller window size for this case)
+        boxesY = int(boxesY) + 1
 
 
 
-    return output
+    for bX in range (0, int(boxesX)):
+        xMin = bX*windowSize
+        xMax = min (xMin + windowSize - 1, height-1)
+        for bY in range(0, int(boxesY)):
+            yMin = bY * windowSize
+            yMax = min(yMin + windowSize - 1, width - 1)
+            currentBoxVolume = (xMax-xMin + 1) * (yMax-yMin + 1) # usually windowSize*windowSize, but it could be different at the edges
+
+            centerX = (xMin + xMax)/ 2
+            centerY = (yMin + yMax) / 2
+
+            samplesWithinHyperbox = 0
+            # now we check everything inside the box
+            for x in range (xMin, xMax+1):
+                for y in range(yMin, yMax + 1):
+                    if (input[x][y] == 255):
+                        samplesWithinHyperbox += 1
+
+             # p(x) = k / (n * V)
+            if (totalSamples * currentBoxVolume) > 0: # just to be safe....
+                estimation = float(samplesWithinHyperbox) / (totalSamples * currentBoxVolume) # should be 0..1
+            else:
+                estimation = 0.0
+
+            #print ("box: " + str(centerX) + "," + str(centerY) + " volume=" + str(currentBoxVolume) + " samples:"+str(samplesWithinHyperbox))
+
+            # generate output: intensity values 0..255
+            for x in range(xMin, xMax+1):
+                for y in range(yMin, yMax+1):
+                    input[x][y] = 255. * estimation
+                    # We're directly replacing the input values here. That's bad practice but works because we don't need to access them again with the Parzen Window Estimator.
+
+
+    return input
 
 
 
-# check how the sampled data flooks like:
-#testdata = racoon_sample (10000)
-#print (testdata)
-# => [0, 0, 0, (...), 0, 0, 88148563, 0, 0, 0, 0, 0, 0, 88148812, 0, 88148123, 0, 0, 0, 0, 0]
-# this is a 1D array with mostly zeros and occasionally grayscale values; the more samples we drew, the more often non-zero entries occur.
+sampleSizes = (10000, 50000, 100000, 200000, 400000)
+windowSizes = (5, 10, 20)
+for s in range(0, len(sampleSizes)):
+    sampledImage0 = raccoon_sample(sampleSizes[s])
+    # Please mind: this image looks similar to a grayscale image. But actually it is just white dots on black background.
+    # The higher a grayscale value of the original racoon image was, the more probable it is to become a white dot in this image.
+    # This 2D array is what we take as samples to reconstruct a PDF with the help of the Parzen Window Estimator
 
+    myplot = plt.imshow(sampledImage0)
+    # plotImage (sampledImage)
+    myplot.figure.savefig("samples=" + str(sampleSizes[s]) + "_raccoon-input.png")
 
-
-
-# reconstruct images from 10k, 50k, 100k, 200k and 400k samples using the Parzen window estimator
-
-windowSize = 100
-recon10k = parzen_reconstruct(racoon_sample (10000), windowSize)
-recon50k = parzen_reconstruct(racoon_sample (50000), windowSize)
-recon100k = parzen_reconstruct(racoon_sample (100000), windowSize)
-recon200k = parzen_reconstruct(racoon_sample (200000),windowSize)
-recon400k = parzen_reconstruct(racoon_sample (400000), windowSize)
-
-
-
-#reshape the reconstructed array to 2D
-output10k=np.reshape(recon10k,(768,1024))
-output50k=np.reshape(recon50k,(768,1024))
-output100k=np.reshape(recon100k,(768,1024))
-output200k=np.reshape(recon200k,(768,1024))
-output400k=np.reshape(recon400k,(768,1024))
-
-
-
-#display
-plt.figure()
-plt.imshow(output10k)
-plt.show()
-
-plt.imshow(output50k)
-plt.show()
-
-plt.imshow(output100k)
-plt.show()
-
-plt.imshow(output200k)
-plt.show()
-
-plt.imshow(output400k)
-plt.show()
-
-## Parzen Window Reconstruction does what it is supposed to do. Result looks bad because we use 1D and could be fixed by applying another way to go 2D-1D.
-
-
-
-
-
-
-
+    for w in range(0, len(windowSizes)):
+        sampledImage = sampledImage0.copy()
+        windowSize = windowSizes[w]
+        reconstructedImage = parzen_reconstruct(sampledImage, windowSize)
+        myplot = plt.imshow(reconstructedImage)
+        # plotImage (reconstructedImage)
+        myplot.figure.savefig("samples=" + str(sampleSizes[s]) + "_raccoon-reconstructed_h=" + str(windowSize) + ".png")
